522d3ba41f29de6b3d68ba08b03c7d9b7d983884
Jacob Lopez <lope0384@umn.edu>
2023-04-17 19:59:21 -0500

    Planned out agent logic

--- game.js
+++ game.js
@@ -45,89 +45,119 @@ function create() {
   this.player.body.setCircle(8, 2, 2);
 
   // Constructor to maintain simulation state
-  this.agents = []; // An array to store all agents
-  this.time = 0;
-  this.tree = null; // your tree structure
+  this.agents = [
+    {
+      x: 280,
+      y: 104,
+      radius: 8,
+      color: 0xff0000,
+      name: "Emma",
+      age: 32,
+      innate: "Friendly and curious",
+      learned: "Excellent cook and craftperson",
+      current: "To find a rare herb for a new dish she wants to try",
+      routines: [
+        "Wakes up early to forage",
+        "Spends the day cooking and crafting",
+        "Visits apothecary to learn",
+        "Ends the day chatting with villagers about their day",
+      ],
+      sprite: 13,
+    },
+    {
+      x: 440,
+      y: 168,
+      radius: 8,
+      color: 0xff0000,
+      name: "John",
+      age: 45,
+      innate: "Protective and stubborn",
+      learned: "Skilled blacksmith",
+      current: "To create a new weapon design for the armory",
+      routines: [
+        "Starts the day at the armory, working on the weapon design",
+        "Goes to the shop to buy supplies",
+        "Spends the afternoon practicing his sword fighting skills",
+        "Ends the day at the pub, telling stories and enjoying a cold beer",
+      ],
+      sprite: 4,
+    },
+    {
+      x: 200,
+      y: 72,
+      radius: 8,
+      color: 0xff0000,
+      name: "Sarah",
+      age: 20,
+      innate: "Shy and introverted",
+      learned: "Excellent healer",
+      current: "To find a new ingredient for a healing salve",
+      routines: [
+        "Starts the day at the apothecary, researching new ingredients and remedies",
+        "Spends the afternoon exploring the forest for herbs and plants",
+        "Ends the day reading books and practicing her healing skills in the apothecary",
+      ],
+      sprite: 7,
+    },
+    {
+      x: 40,
+      y: 88,
+      radius: 8,
+      color: 0xff0000,
+      name: "William",
+      age: 55,
+      innate: "Grumpy and solitary",
+      learned: "Skilled hunter",
+      current: "To catch a rare deer he's been tracking for weeks",
+      routines: [
+        "Starts the day early in the forest, tracking and hunting animals",
+        "Spends the afternoon in his shack, cleaning and cooking the game",
+        "Ends the day drinking alone in the pub, muttering about the other villagers",
+      ],
+      sprite: 10,
+    },
+  ]; // An array to store all agents
+  this.time = this.time.now; // A variable to store the current time, may eventually rely on larger set of time blocks
+  this.tree = {
+    shop: {
+      chest: { state: "", location: [100, 100] },
+      table: { state: "", location: [120, 100] },
+      bed: { state: "", location: [140, 100] },
+    },
+    apothecary: {
+      flame: { state: "", location: [200, 200] },
+      table: { state: "", location: [220, 200] },
+      bed: { state: "", location: [240, 200] },
+      drawer: { state: "", location: [260, 200] },
+    },
+    shack: {
+      bed: { state: "", location: [300, 300] },
+    },
+    armory: {
+      weaponRoom: {
+        Statue: { state: "", location: [400, 400] },
+        Drawer: { state: "", location: [420, 400] },
+        Pot: { state: "", location: [440, 400] },
+        Table: { state: "", location: [460, 400] },
+        Bed: { state: "", location: [480, 400] },
+      },
+      armorRoom: {
+        Table: { state: "", location: [500, 500] },
+        Bed: { state: "", location: [520, 500] },
+        Chest1: { state: "", location: [540, 500] },
+        Chest2: { state: "", location: [560, 500] },
+      },
+    },
+    forest: { state: "", location: [600, 600] },
+    mushroomPatch: { state: "", location: [700, 700] },
+    mailbox: { state: "", location: [800, 800] },
+    herbPatch: { state: "", location: [900, 900] },
+    well: { state: "", location: [1000, 1000] },
+  }; // Tree structure to store the state of the world TODO: Put actual locations
 
   //Create the map and wall collision layer
   wallsLayer.setCollisionBetween(0, 1000);
   this.physics.add.collider(this.player, wallsLayer); // collide group with walls
-
-  // Add agents to the array
-  const emma = {
-    x: 280, // Example x coordinate
-    y: 104, // Example y coordinate
-    radius: 8, // Example radius
-    color: 0xff0000, // Example color in hex format
-    name: "Emma",
-    age: 32,
-    innate: "Friendly and curious",
-    learned: "Excellent cook and craftperson",
-    current: "To find a rare herb for a new dish she wants to try",
-    routines: [
-      "Wakes up early to forage",
-      "Spends the day cooking and crafting",
-      "Visits apothecary to learn",
-      "Ends the day chatting with villagers about their day",
-    ],
-    sprite: 13,
-  };
-  this.agents.push(emma);
-  const john = {
-    x: 440,
-    y: 168,
-    radius: 8,
-    color: 0xff0000,
-    name: "John",
-    age: 45,
-    innate: "Protective and stubborn",
-    learned: "Skilled blacksmith",
-    current: "To create a new weapon design for the armory",
-    routines: [
-      "Starts the day at the armory, working on the weapon design",
-      "Goes to the shop to buy supplies",
-      "Spends the afternoon practicing his sword fighting skills",
-      "Ends the day at the pub, telling stories and enjoying a cold beer",
-    ],
-    sprite: 4,
-  };
-  this.agents.push(john);
-  const sarah = {
-    x: 200,
-    y: 72,
-    radius: 8,
-    color: 0xff0000,
-    name: "Sarah",
-    age: 20,
-    innate: "Shy and introverted",
-    learned: "Excellent healer",
-    current: "To find a new ingredient for a healing salve",
-    routines: [
-      "Starts the day at the apothecary, researching new ingredients and remedies",
-      "Spends the afternoon exploring the forest for herbs and plants",
-      "Ends the day reading books and practicing her healing skills in the apothecary",
-    ],
-    sprite: 7,
-  };
-  this.agents.push(sarah);
-  const william = {
-    x: 40,
-    y: 88,
-    radius: 8,
-    color: 0xff0000,
-    name: "William",
-    age: 55,
-    innate: "Grumpy and solitary",
-    learned: "Skilled hunter",
-    current: "To catch a rare deer he's been tracking for weeks",
-    routines: [
-      "Starts the day early in the forest, tracking and hunting animals",
-      "Spends the afternoon in his shack, cleaning and cooking the game",
-      "Ends the day drinking alone in the pub, muttering about the other villagers",
-    ],
-    sprite: 10,
-  };
-  this.agents.push(william);
 
   // Check for user input
   this.cursors = this.input.keyboard.createCursorKeys();
@@ -165,6 +195,9 @@ function create() {
     frameRate: 3,
     repeat: -1,
   });
+
+  // Create the grid
+  this.add.grid(20, 20, 1000, 1000, 16, 16, 0x000000, 0.5, 0x000000, 0.5);
 }
 
 function update() {
@@ -191,45 +224,21 @@ function update() {
   }
 
   // Keep track of time
-  const currentTime = this.time.now;
+  this.time += 1;
 
-  // Store objects in a tree structure
-  const tree = {
-    shop: {
-      chest: { state: "" },
-      table: { state: "" },
-      bed: { state: "" },
-    },
-    apothecary: {
-      flame: { state: "" },
-      table: { state: "" },
-      bed: { state: "" },
-      drawer: { state: "" },
-    },
-    shack: {
-      bed: { state: "" },
-    },
-    armory: {
-      weaponRoom: {
-        Statue: { state: "" },
-        Drawer: { state: "" },
-        Pot: { state: "" },
-        Table: { state: "" },
-        Bed: { state: "" },
-      },
-      armorRoom: {
-        Table: { state: "" },
-        Bed: { state: "" },
-        Chest1: { state: "" },
-        Chest2: { state: "" },
-      },
-    },
-  };
-
-  // Render all agents in the array as circles
+  // Update Agents
   for (const agent of this.agents) {
+    // Render agent in current location
     this.add.sprite(agent.x, agent.y, "player", agent.sprite);
-  }
 
-  // Check for agent updates
+    // Prompt agent for statement describing their current action
+
+    // Check in with memory/brain store
+
+    // Give feedback and reflection on action
+
+    // Convert to concrete action by calculating the path to location
+
+    // Execute action
+  }
 }
